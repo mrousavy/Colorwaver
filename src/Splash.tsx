10dd2de25094d40a4b076e81f2ae9cb569983068
Marc Rousavy <marcrousavy@hotmail.com>
2021-08-28 10:00:59 +0200

    Add Splash

--- src/Splash.tsx
+++ src/Splash.tsx
@@ -1,9 +1,11 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useCallback, useEffect, useState} from 'react';
-import {Alert, Linking, Pressable, StyleSheet, Text, View} from 'react-native';
+import {Alert, Image, Linking, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Camera} from 'react-native-vision-camera';
 import {Routes} from './Routes';
+
+const IMAGE = require('./assets/11.png');
 
 type Props = NativeStackScreenProps<Routes, 'Splash'>;
 
@@ -46,9 +48,18 @@ export function Splash({navigation}: Props) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Pressable onPress={requestPermission}>
-        <Text>Request Permission</Text>
-      </Pressable>
+      <Image source={IMAGE} style={styles.backgroundImage} />
+
+      <Text style={styles.header}>Welcome to{'\n'}Colorwaver.</Text>
+
+      <View style={styles.permissionsContainer}>
+        <Text style={styles.permissionText}>
+          Colorwaver needs <Text style={styles.bold}>Camera permission</Text>.{' '}
+          <Text style={styles.hyperlink} onPress={requestPermission}>
+            Grant
+          </Text>
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -58,5 +69,30 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 15,
     paddingVertical: 15,
+  },
+  backgroundImage: {
+    position: 'absolute',
+    opacity: 0.4,
+    bottom: 0,
+    left: 0,
+  },
+  header: {
+    fontSize: 38,
+    fontWeight: 'bold',
+    maxWidth: '80%',
+  },
+  permissionsContainer: {
+    marginTop: 30,
+  },
+  permissionText: {
+    fontSize: 18,
+    maxWidth: '80%',
+  },
+  hyperlink: {
+    color: '#007aff',
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
   },
 });

bad417006e0dd6986b8b923e80905515d4b7e3fa
Marc Rousavy <marcrousavy@hotmail.com>
2021-08-28 09:37:17 +0200

    Install reanimated and VisionCamera

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'react-native-reanimated/plugin',
+      {
+        globals: [],
+      },
+    ],
+  ],
 };

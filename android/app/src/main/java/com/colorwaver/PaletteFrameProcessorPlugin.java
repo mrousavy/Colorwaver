7c5510a43467d77cabfd0bcf8809b190272153c7
Marc Rousavy <marcrousavy@hotmail.com>
2021-08-28 15:45:27 +0200

    Use Palette's `getLightVibrantColor` instead of custom `calculateAverageColor`

--- android/app/src/main/java/com/colorwaver/PaletteFrameProcessorPlugin.java
+++ android/app/src/main/java/com/colorwaver/PaletteFrameProcessorPlugin.java
@@ -5,12 +5,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.Image;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.camera.core.ImageProxy;
 import androidx.palette.graphics.Palette;
-
 import com.colorwaver.utils.YuvToRgbConverter;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
@@ -35,12 +33,7 @@ public class PaletteFrameProcessorPlugin extends FrameProcessorPlugin {
 
         Palette palette = new Palette.Builder(bitmap).generate();
 
-        int pixelSpacing = 5;
-        if (params.length > 0 && params[0] instanceof Integer) {
-            pixelSpacing = (Integer) params[0];
-        }
-
-        int average = calculateAverageColor(bitmap, pixelSpacing);
+        int average = palette.getLightVibrantColor(DEFAULT_COLOR);
         int vibrant = palette.getVibrantColor(DEFAULT_COLOR);
         int darkVibrant = palette.getDarkVibrantColor(DEFAULT_COLOR);
         int dominant = palette.getDominantColor(DEFAULT_COLOR);
@@ -51,34 +44,6 @@ public class PaletteFrameProcessorPlugin extends FrameProcessorPlugin {
         result.putString("detail", rgbIntToHexString(average));
         result.putString("background", rgbIntToHexString(dominant));
         return result;
-    }
-
-    /**
-     * https://gist.github.com/maxjvh/a6ab15cbba9c82a5065d
-     * pixelSpacing tells how many pixels to skip each pixel.
-     * If pixelSpacing > 1: the average color is an estimate, but higher values mean better performance.
-     * If pixelSpacing == 1: the average color will be the real average.
-     * If pixelSpacing < 1: the method will most likely crash (don't use values below 1).
-     */
-    private int calculateAverageColor(@NonNull Bitmap bitmap, int pixelSpacing) {
-        int R = 0;
-        int G = 0;
-        int B = 0;
-        int height = bitmap.getHeight();
-        int width = bitmap.getWidth();
-        int n = 0;
-        int[] pixels = new int[width * height];
-
-        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
-
-        for (int i = 0; i < pixels.length; i += pixelSpacing) {
-            int color = pixels[i];
-            R += Color.red(color);
-            G += Color.green(color);
-            B += Color.blue(color);
-            n++;
-        }
-        return Color.rgb(R / n, G / n, B / n);
     }
 
     private String rgbIntToHexString(int rgb) {

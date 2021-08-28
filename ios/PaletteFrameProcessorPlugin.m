a21ad836c603b3c923e3ca6ce19ebe380211dd00
Marc Rousavy <marcrousavy@hotmail.com>
2021-08-28 10:24:55 +0200

    Create `PaletteFrameProcessorPlugin` base

---
+++ ios/PaletteFrameProcessorPlugin.m
@@ -0,0 +1,12 @@
+//
+//  PaletteFrameProcessorPlugin.m
+//  Colorwaver
+//
+//  Created by Marc Rousavy on 28.08.21.
+//
+
+#import <Foundation/Foundation.h>
+#import <VisionCamera/FrameProcessorPlugin.h>
+
+@interface VISION_EXPORT_SWIFT_FRAME_PROCESSOR(getColorPalette, PaletteFrameProcessorPlugin)
+@end
---
+++ ios/PaletteFrameProcessorPlugin.swift
@@ -0,0 +1,27 @@
+//
+//  PaletteFrameProcessorPlugin.swift
+//  Colorwaver
+//
+//  Created by Marc Rousavy on 28.08.21.
+//
+
+import Foundation
+import UIImageColors
+
+@objc(PaletteFrameProcessorPlugin)
+public class PaletteFrameProcessorPlugin: NSObject, FrameProcessorPluginBase {
+  private static let context = CIContext(options: nil)
+  
+  @objc
+  public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
+    let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer)!
+    let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+
+    let cgImage = context.createCGImage(ciImage, from: ciImage.extent)!
+    let image = UIImage(cgImage: cgImage)
+    let colors = image.getColors()
+    
+    // TODO: Convert colors
+    return []
+  }
+}

ffeaacb16573ccb281b26fd87d87b525caf92f0c
Bartosz Kaszubowski <gosimek@gmail.com>
2021-08-30 12:24:00 +0200

    refactor, fix iOS build, introduce ColorTile (#2)
    * refactor, fix iOS build, introduce ColorTile
    
    * small cleanup
    
    * styles simplification

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,25 @@
 import React, {useCallback, useState} from 'react';
-import {
-  Dimensions,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  TextInputProps,
-  View,
-} from 'react-native';
+import {Pressable, StyleSheet, View} from 'react-native';
 import {
   Camera,
   CameraRuntimeError,
   useCameraDevices,
   useFrameProcessor,
 } from 'react-native-vision-camera';
-import Reanimated, {
-  useAnimatedProps,
-  useAnimatedStyle,
-} from 'react-native-reanimated';
+import Reanimated from 'react-native-reanimated';
 import {getColorPalette} from './getColorPalette';
 import {useSharedValue} from 'react-native-reanimated';
-import {useAnimatedColor} from './useAnimatedColor';
+import ColorTile from './components/ColorTile';
 
 Reanimated.addWhitelistedNativeProps({
   text: true,
 });
-const ReanimatedTextInput = Reanimated.createAnimatedComponent(TextInput);
 
-const SCREEN_HEIGHT = Dimensions.get('window').height;
 const DEFAULT_COLOR = '#000000';
 
 export function App() {
   const [isActive, setIsActive] = useState(true);
+
   const devices = useCameraDevices('wide-angle-camera');
   const device = devices.back;
   const primaryColor = useSharedValue(DEFAULT_COLOR);
@@ -54,53 +42,6 @@ export function App() {
     backgroundColor.value = colors.background;
     detailColor.value = colors.detail;
   }, []);
-
-  const topLeftColor = useAnimatedColor(primaryColor);
-  const topRightColor = useAnimatedColor(secondaryColor);
-  const bottomLeftColor = useAnimatedColor(backgroundColor);
-  const bottomRightColor = useAnimatedColor(detailColor);
-
-  const tileTopLeftStyle = useAnimatedStyle(
-    () => ({
-      backgroundColor: topLeftColor.value,
-    }),
-    [],
-  );
-  const tileTopRightStyle = useAnimatedStyle(
-    () => ({
-      backgroundColor: topRightColor.value,
-    }),
-    [],
-  );
-  const tileBottomLeftStyle = useAnimatedStyle(
-    () => ({
-      backgroundColor: bottomLeftColor.value,
-    }),
-    [],
-  );
-  const tileBottomRightStyle = useAnimatedStyle(
-    () => ({
-      backgroundColor: bottomRightColor.value,
-    }),
-    [],
-  );
-
-  // @ts-expect-error `text` is a hidden native prop
-  const primaryTextProps = useAnimatedProps<TextInputProps>(() => ({
-    text: primaryColor.value,
-  }));
-  // @ts-expect-error `text` is a hidden native prop
-  const secondaryTextProps = useAnimatedProps<TextInputProps>(() => ({
-    text: secondaryColor.value,
-  }));
-  // @ts-expect-error `text` is a hidden native prop
-  const backgroundTextProps = useAnimatedProps<TextInputProps>(() => ({
-    text: backgroundColor.value,
-  }));
-  // @ts-expect-error `text` is a hidden native prop
-  const detailTextProps = useAnimatedProps<TextInputProps>(() => ({
-    text: detailColor.value,
-  }));
 
   const onPressIn = useCallback(() => {
     setIsActive(false);
@@ -127,38 +68,13 @@ export function App() {
         style={styles.camera}
         onError={onCameraError}
         onInitialized={onCameraInitialized}
-        frameProcessorFps={5}
+        frameProcessorFps={3}
       />
-
       <View style={styles.palettes}>
-        <Reanimated.View style={[styles.tileTopLeft, tileTopLeftStyle]}>
-          <Text style={styles.text}>Primary</Text>
-          <ReanimatedTextInput
-            style={styles.smallerText}
-            animatedProps={primaryTextProps}
-          />
-        </Reanimated.View>
-        <Reanimated.View style={[styles.tileTopRight, tileTopRightStyle]}>
-          <Text style={styles.text}>Secondary</Text>
-          <ReanimatedTextInput
-            style={styles.smallerText}
-            animatedProps={secondaryTextProps}
-          />
-        </Reanimated.View>
-        <Reanimated.View style={[styles.tileBottomLeft, tileBottomLeftStyle]}>
-          <Text style={styles.text}>Background</Text>
-          <ReanimatedTextInput
-            style={styles.smallerText}
-            animatedProps={backgroundTextProps}
-          />
-        </Reanimated.View>
-        <Reanimated.View style={[styles.tileBottomRight, tileBottomRightStyle]}>
-          <Text style={styles.text}>Detail</Text>
-          <ReanimatedTextInput
-            style={styles.smallerText}
-            animatedProps={detailTextProps}
-          />
-        </Reanimated.View>
+        <ColorTile name="Primary" color={primaryColor} />
+        <ColorTile name="Secondary" color={secondaryColor} />
+        <ColorTile name="Background" color={backgroundColor} />
+        <ColorTile name="Detail" color={detailColor} />
       </View>
     </Pressable>
   );
@@ -178,51 +94,8 @@ const styles = StyleSheet.create({
   palettes: {
     left: 0,
     bottom: 0,
-    right: 0,
-    height: SCREEN_HEIGHT / 5,
+    height: 100,
     flexDirection: 'row',
     backgroundColor: 'black',
-  },
-  tileTopLeft: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  tileTopRight: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  tileBottomLeft: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  tileBottomRight: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    textShadowColor: 'black',
-    textShadowOffset: {
-      height: 0,
-      width: 0,
-    },
-    textShadowRadius: 2,
-    color: 'white',
-  },
-  smallerText: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    textShadowColor: 'black',
-    textShadowOffset: {
-      height: 0,
-      width: 0,
-    },
-    textShadowRadius: 2,
-    color: 'white',
   },
 });

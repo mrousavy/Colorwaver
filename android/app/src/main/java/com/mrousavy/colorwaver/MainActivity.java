6a4783c4f3939794b3953a318f818cae2eaf21a6
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-21 16:51:33 +0200

    Fix RNGH installation on Android

--- android/app/src/main/java/com/mrousavy/colorwaver/MainActivity.java
+++ android/app/src/main/java/com/mrousavy/colorwaver/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mrousavy.colorwaver;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
 import com.mrousavy.camera.frameprocessor.FrameProcessorRuntimeManager;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import android.os.Bundle;
 
@@ -15,6 +18,16 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Colorwaver";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 
   @Override

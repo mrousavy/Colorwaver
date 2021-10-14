308ebf68ab4b4c1d2bc2db8ac88776d15d63484f
Marc Rousavy <marcrousavy@hotmail.com>
2021-10-14 17:10:33 +0200

    fallback

--- scripts/release-ios.sh
+++ scripts/release-ios.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+trap "exit" INT
+for ((i=0; i < $srccount; i++)); do
+    echo -e "\"${src[$i]}\" will be synchronized to \"${dest[$i]}\""
+    echo -e $'Press any key to continue or Ctrl+C to exit...\n'
+    read -rs -n1
+    rsync ${opt1} ${opt2} ${opt3} ${src[$i]} ${dest[$i]}
+done
+
 cd "$(dirname "$0")"
 cd ..
 
@@ -12,7 +20,7 @@ fi
 
 if [ -z "$(git status --porcelain)" ]; then
   # Working directory clean
-  echo 'Clean working directory';
+  ;
 else
   # Uncommitted changes
   echo 'Working directory not clean! Commit/Revert changes and try again.';

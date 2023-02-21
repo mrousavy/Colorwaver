8c2630eee4d2e94fdc754fc948ef5026d85ac2ef
Marc Rousavy <me@mrousavy.com>
2023-02-21 21:32:01 +0100

    Install Reanimated v3

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'react-native-reanimated/plugin',
-      {
-        globals: ['__getColorPalette'],
-      },
-    ],
-  ],
+  plugins: ['react-native-reanimated/plugin', 'react-native-worklets/plugin'],
 };
